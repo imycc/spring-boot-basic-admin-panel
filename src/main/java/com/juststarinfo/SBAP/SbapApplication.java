7d78db58ebe94ecf40a2757cd3ccac426659ffeb
imycc <boyz357159@gmail.com>
2020-08-19 18:14:14 +0800

    init2

---
+++ src/main/java/com/juststarinfo/SBAP/SbapApplication.java
@@ -0,0 +1,13 @@
+package com.juststarinfo.SBAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juststarinfo/SBAP/SbapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juststarinfo.SBAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
