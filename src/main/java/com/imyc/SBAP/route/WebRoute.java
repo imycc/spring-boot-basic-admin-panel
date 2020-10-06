b106c43c04440cb022bacc50abc3e2b1909bf48d
imycc <boyz357159@gmail.com>
2020-10-06 19:24:03 +0800

    todo: css js

--- src/main/java/com/imyc/SBAP/route/WebRoute.java
+++ src/main/java/com/imyc/SBAP/route/WebRoute.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebRoute {
 
-	@GetMapping("/admin")
+	@GetMapping("/")
     public String home() { return "admin-panel/index"; }
 	
 	@GetMapping("/login")
