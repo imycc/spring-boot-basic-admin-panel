9716ec668a656c90be9a26b15a6dba77e524f7a9
imycc <boyz357159@gmail.com>
2020-10-06 14:58:03 +0800

    asd

---
+++ src/main/java/com/imyc/SBAP/http/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.imyc.SBAP.http.user.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.imyc.SBAP.http.user.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
