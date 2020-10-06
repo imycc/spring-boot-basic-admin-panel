8e1c6bae5db479a34ac5a180da42ef8ef67bc70c
imycc <boyz357159@gmail.com>
2020-10-06 18:00:31 +0800

    remove template

--- src/main/java/com/imyc/SBAP/SbapApplication.java
+++ src/main/java/com/imyc/SBAP/SbapApplication.java
@@ -1,17 +1,39 @@
 package com.imyc.SBAP;
 
+import java.util.Collections;
+
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SbapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbapApplication.class, args);
-		
+
 //		Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:8889/sbap", "root", "root").load();
 //		flyway.migrate();
 	}
 
+	@Bean
+	public FilterRegistrationBean simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// *** URL below needs to match the Vue client URL and port ***
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 }
--- src/main/java/com/imyc/SBAP/route/WebRoute.java
+++ src/main/java/com/imyc/SBAP/route/WebRoute.java
@@ -2,32 +2,27 @@ package com.imyc.SBAP.route;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebRoute {
 
-	@GetMapping("/")
+	@GetMapping("/admin")
     public String home() { return "admin-panel/index"; }
 	
-	@GetMapping("/login.html")
-	public String login() { return "admin-panel/login"; }
+	@GetMapping("/login")
+	public String login() { return "admin-panel/auth/login"; }
 	
-	@GetMapping("/register.html")
-	public String register() { return "admin-panel/register"; }
+	@GetMapping("/register")
+	public String register() { return "admin-panel/auth/register"; }
 	
-	@GetMapping("/index.html")
-    public String index() { return "admin-panel/index"; }
-
-	@GetMapping("/pages/ui-features/buttons.html")
-	public String buttons() { return "admin-panel/ui-features/buttons"; }
-	
-	@GetMapping("/error-403.html")
+	@GetMapping("/error-403")
 	public String error403() { return "admin-panel/error/error-403"; }
 	
-	@GetMapping("/error-404.html")
+	@GetMapping("/error-404")
 	public String error404() { return "admin-panel/error/error-404"; }
 	
-	@GetMapping("/error-500.html")
+	@GetMapping("/error-500")
 	public String error500() { return "admin-panel/error/error-500"; }
 	
 	
