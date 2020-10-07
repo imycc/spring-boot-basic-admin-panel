fa54cbddf2af1235470b2e0fa08704e45bf8a0f6
imycc <boyz357159@gmail.com>
2020-10-07 17:37:52 +0800

    menu update

--- src/main/java/com/imyc/SBAP/route/Web.java
+++ src/main/java/com/imyc/SBAP/route/Web.java
@@ -7,23 +7,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Web {
 
-	@GetMapping("/")
-    public String home() { return "admin-panel/index"; }
-	
+	@GetMapping("/dashboard")
+	public String home() {
+		return "admin-panel/index";
+	}
+
 	@GetMapping("/login")
-	public String login() { return "admin-panel/auth/login"; }
-	
+	public String login() {
+		return "admin-panel/auth/login";
+	}
+
 	@GetMapping("/register")
-	public String register() { return "admin-panel/auth/register"; }
-	
+	public String register() {
+		return "admin-panel/auth/register";
+	}
+
+	@GetMapping("/role")
+	public String role() {
+		return "admin-panel/role/index";
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		return "admin-panel/user/index";
+	}
+
 	@GetMapping("/error-403")
-	public String error403() { return "admin-panel/error/error-403"; }
-	
+	public String error403() {
+		return "admin-panel/error/error-403";
+	}
+
 	@GetMapping("/error-404")
-	public String error404() { return "admin-panel/error/error-404"; }
-	
+	public String error404() {
+		return "admin-panel/error/error-404";
+	}
+
 	@GetMapping("/error-500")
-	public String error500() { return "admin-panel/error/error-500"; }
-	
-	
+	public String error500() {
+		return "admin-panel/error/error-500";
+	}
+
 }
